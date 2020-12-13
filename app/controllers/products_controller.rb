1850f09b6feea011c0c86d1799e364516da03948
madelinemc <madeline.coons@gmail.com>
2020-12-13 15:09:50 -0600

    commit tree cbbb5e2bd0137fb8c22e25d41d1e8d0692cf8f39

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,12 @@
+class ProductsController < ApplicationController
+
+    def index
+    end
+
+    def add
+        item = params[:product]
+        cart << item
+        render 'index'
+    end
+
+end
